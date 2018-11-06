3458d286378ae567f80465e6c60ba0f732efe26a
xyj1209 <15817061252@163.com>
2018-11-06 15:07:55 +0800

    修改分数限制范围

--- src/main/java/org/javatribe/score/po/Score.java
+++ src/main/java/org/javatribe/score/po/Score.java
@@ -6,8 +6,8 @@ import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 public class Score extends ScoreKey implements Serializable {
-    @Max(value = 10,message = "评分范围0-10")
-    @Min(value = 0,message = "评分范围0-10")
+    @Max(value = 100,message = "评分范围0-100")
+    @Min(value = 0,message = "评分范围0-100")
     private Double scoreNum;
 
     public Double getScoreNum() {
